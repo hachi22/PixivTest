dfb937fa70b0c42c52b1c2d2bd51e7359e55c7bc
Miquel <miquel.rodriguez.7e3@itb.cat>
2021-02-26 00:04:26 +0100

    Inicialización de los recycler Views en los fragments y creacion de la clase FireBaseHelper

--- app/src/main/java/cat/itb/pixiv/Fragments/HomeFragments/FragmentHomeIllustrations.java
+++ app/src/main/java/cat/itb/pixiv/Fragments/HomeFragments/FragmentHomeIllustrations.java
@@ -3,16 +3,33 @@ package cat.itb.pixiv.Fragments.HomeFragments;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.provider.ContactsContract;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
+import com.firebase.ui.database.FirebaseRecyclerOptions;
+import com.google.firebase.database.DatabaseReference;
+
+import cat.itb.pixiv.Adapater.AdapterIlustrationsRecomended;
+import cat.itb.pixiv.Adapater.AdapterPopularLives;
+import cat.itb.pixiv.Adapater.AdapterRankingIM;
+import cat.itb.pixiv.ClassesModels.ImatgesP;
+import cat.itb.pixiv.FireBase.FireBaseHelper;
 import cat.itb.pixiv.R;
 
 public class FragmentHomeIllustrations extends Fragment {
 
-
+    RecyclerView recyclerView;
+    AdapterRankingIM adapterRanking;
+    AdapterIlustrationsRecomended adapterRecomended;
+    AdapterPopularLives adapterPopularLives;
+    DatabaseReference myRef;
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,7 +38,36 @@ public class FragmentHomeIllustrations extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home_illustrations, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_home_illustrations, container, false);
+        FireBaseHelper.obtenerReferencia();
+
+
+        recyclerView = rootView.findViewById(R.id.recycler_view_illustrations_ranking);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+        FirebaseRecyclerOptions<ImatgesP> options = new FirebaseRecyclerOptions.Builder<ImatgesP>()
+                .setQuery(FireBaseHelper.getReferenceRanking(), ImatgesP.class).build();
+        adapterRanking = new AdapterRankingIM(options);
+        adapterRanking.setContext(getContext());
+        recyclerView.setAdapter(adapterRanking);
+
+
+        recyclerView = rootView.findViewById(R.id.recycler_view_illustrations_popular_lives);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+        FirebaseRecyclerOptions<ImatgesP> options2 = new FirebaseRecyclerOptions.Builder<ImatgesP>()
+                .setQuery(FireBaseHelper.getReferencePopularLives(), ImatgesP.class).build();
+        adapterPopularLives = new AdapterPopularLives(options2);
+        adapterPopularLives.setContext(getContext());
+        recyclerView.setAdapter(adapterPopularLives);
+
+
+        recyclerView = rootView.findViewById(R.id.recycler_view_illustrations_recommended);
+        recyclerView.setLayoutManager(new GridLayoutManager(getContext(),2));
+        FirebaseRecyclerOptions<ImatgesP> options3 = new FirebaseRecyclerOptions.Builder<ImatgesP>()
+                .setQuery(FireBaseHelper.getReferenceRecommendedIllustrations(), ImatgesP.class).build();
+        adapterRecomended = new AdapterIlustrationsRecomended(options3);
+        adapterRecomended.setContext(getContext());
+        recyclerView.setAdapter(adapterRecomended);
+
+        return rootView;
     }
 }
--- app/src/main/java/cat/itb/pixiv/Fragments/HomeFragments/FragmentHomeNovels.java
+++ app/src/main/java/cat/itb/pixiv/Fragments/HomeFragments/FragmentHomeNovels.java
@@ -3,14 +3,31 @@ package cat.itb.pixiv.Fragments.HomeFragments;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.firebase.ui.database.FirebaseRecyclerOptions;
+import com.google.firebase.database.DatabaseReference;
+
+import cat.itb.pixiv.Adapater.AdapterMangaRecommended;
+import cat.itb.pixiv.Adapater.AdapterNovelsRecommended;
+import cat.itb.pixiv.Adapater.AdapterPixivVision;
+import cat.itb.pixiv.Adapater.AdapterRankingIM;
+import cat.itb.pixiv.Adapater.AdapterRankingNovels;
+import cat.itb.pixiv.ClassesModels.ImatgesP;
+import cat.itb.pixiv.FireBase.FireBaseHelper;
 import cat.itb.pixiv.R;
 
 public class FragmentHomeNovels extends Fragment {
+
+    RecyclerView recyclerView;
+    AdapterNovelsRecommended adapterRecommended;
+    AdapterRankingNovels adapterRanking;
+    DatabaseReference myRef;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -20,7 +37,24 @@ public class FragmentHomeNovels extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home_novels, container, false);
+        View rootView =  inflater.inflate(R.layout.fragment_home_novels, container, false);
+
+        recyclerView = rootView.findViewById(R.id.recycler_view_novels_ranking);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+        FirebaseRecyclerOptions<ImatgesP> options = new FirebaseRecyclerOptions.Builder<ImatgesP>()
+                .setQuery(FireBaseHelper.getReferenceRanking(), ImatgesP.class).build();
+        adapterRanking = new AdapterRankingNovels(options);
+        adapterRanking.setContext(getContext());
+        recyclerView.setAdapter(adapterRanking);
+
+        recyclerView = rootView.findViewById(R.id.recycler_view_novels_recommended);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+        FirebaseRecyclerOptions<ImatgesP> options2 = new FirebaseRecyclerOptions.Builder<ImatgesP>()
+                .setQuery(FireBaseHelper.getReferenceRecommendedNovels(), ImatgesP.class).build();
+        adapterRecommended = new AdapterNovelsRecommended(options2);
+        adapterRecommended.setContext(getContext());
+        recyclerView.setAdapter(adapterRecommended);
+
+        return rootView;
     }
 }
