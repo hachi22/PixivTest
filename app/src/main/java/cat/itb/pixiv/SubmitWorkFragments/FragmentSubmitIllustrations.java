78cede441a6ab84ad983a138fb63831bcce27a55
hachi22 <adrian.nunyez.7e3@itb.cat>
2021-03-18 17:37:44 +0100

    Falta lo del radioButton azul/negro

---
+++ app/src/main/java/cat/itb/pixiv/SubmitWorkFragments/FragmentSubmitIllustrations.java
@@ -0,0 +1,25 @@
+package cat.itb.pixiv.SubmitWorkFragments;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import cat.itb.pixiv.R;
+
+public class FragmentSubmitIllustrations extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.submit_work_illustrations_manga, container, false);
+
+
+
+        return v;
+    }
+}
